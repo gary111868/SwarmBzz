dc95c0c250bedc25f8d2bd9f0ea92bb965a0c818
Gary <62193371+gary111868@users.noreply.github.com>
2025-08-03 02:36:04 +0700

    Add main.go
    Co-authored-by: Hajigur <66867581+hajigur69@users.noreply.github.com>

---
+++ cmd/swarmbzz/main.go
@@ -0,0 +1,23 @@
+// cmd/swarmbzz/main.go
+package main
+
+import (
+	"flag"
+	"log"
+	"os"
+
+	"swarmbzz/internal/swarmbzz"
+)
+
+func main() {
+	verbose := flag.Bool("verbose", false, "Enable verbose logging")
+	input := flag.String("input", "", "Input file path")
+	output := flag.String("output", "", "Output file path")
+	flag.Parse()
+
+	app := swarmbzz.NewApp(*verbose)
+	if err := app.Run(*input, *output); err != nil {
+		log.Printf("Error: %v", err)
+		os.Exit(1)
+	}
+}
